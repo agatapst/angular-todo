89917b9a32cc7a9ddbcce7e87509174e8dba8a20
Agata Stawarz <agata.stawarz94@gmail.com>
2019-06-28 21:55:13 +0200

    Add basic text structure, todo class and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title = 'TODO app';
+  subtitle = "What are you up to?"
 }
